5594953b40b21584b13d429dfc3e15b82864964b
Lucas Gomes <lukashitoh@gmail.com>
2025-02-10 18:35:01 -0200

    Nova feat em script.js e arrumando algumas coisas

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 const caixaParaDigitarNomeDaCidade = document.getElementById('caixaParaDigitarNomeDaCidade')
 const listaDeSugestoes = document.getElementById('listaDeSugestoes')
 
-let nomeDaCidade;
-let idDaCidade;
+const dataDaAtualiazacao = document.getElementById('data')
+const localizacao = document.getElementById('local')
+const descricao = document.getElementById('descricao')
+const temperaturaMaxima = document.getElementById('tempmax')
+const temperaturaMinima = document.getElementById('tempmin')
+
 let timeout;
+let diasDePrevisao = 6
+let tempoDeResposta = 1000
 
 caixaParaDigitarNomeDaCidade.addEventListener('input', () => {
-    clearTimeout(timeout)
+    clearTimeout(timeout) // Utilizado para limpar o Timeout e q possa recomeçar o delay novamente.
 
     let nomeDaCidade = caixaParaDigitarNomeDaCidade.value
 
-    timeout = setTimeout(() => {
-        let urlDaAPI = `https://brasilapi.com.br/api/cptec/v1/cidade/${nomeDaCidade}`
-
-        fetch(urlDaAPI)
+    timeout = setTimeout(() => { // Utilizado para dar um delay na sugestão quando o usuario pressionar uma tecla
+        let urlDaAPIBuscarLocalidades = `https://brasilapi.com.br/api/cptec/v1/cidade/${nomeDaCidade}`
+        // Usado para pegar o ID de uma cidade para depois ser utilizado para pegar as informações do tempo dessa cidade
+        fetch(urlDaAPIBuscarLocalidades)
             .then(response => response.json())
             .then(data => {
-
-                if(data){
+                if (data) {
                     listaDeSugestoes.innerHTML = ''
                     data.forEach(d => {
                         let li = document.createElement('li')
-                        li.textContent = ''
                         li.textContent = d.nome
                         listaDeSugestoes.append(li)
-                        li.onclick = ()=>{
-                            idDaCidade = d.id
+                        li.onclick = () => {
+                            let idDaCidade = d.id
                             listaDeSugestoes.innerHTML = ''
+                            let urlDaAPIPrevisaoMeteorologica = `https://brasilapi.com.br/api/cptec/v1/clima/previsao/${idDaCidade}/${diasDePrevisao}`
+
+                            fetch(urlDaAPIPrevisaoMeteorologica)
+                                .then(response => response.json())
+                                .then(data => {
+                                    dataDaAtualiazacao.innerHTML = `Atualizado em ${data.atualizado_em}`
+                                    localizacao.innerHTML = `${data.estado}, ${data.cidade}`
+                                    temperaturaMaxima.innerHTML = `Dia ${data.clima[0].max}°C`
+                                    temperaturaMinima.innerHTML = `Noite ${data.clima[0].min}°C`
+                                    descricao.innerHTML = data.clima[0].condicao_desc
+                                })
+                                .catch(error => console.log('Error', error))
                         }
                     });
                 }
-            }
-            )
+            })
             .catch(error => console.error('Erro:', error))
-    }, 1000)
+    }, tempoDeResposta)
 
 })
